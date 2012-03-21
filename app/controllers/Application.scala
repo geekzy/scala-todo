4f610869505ce55f88cd238803dcf3fed4fee43e
Imam Kurniawan <geekzy@gmail.com>
2012-03-21 16:47:23 +0700

    Initial

---
+++ app/controllers/Application.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models._
+
+object Application extends Controller {
+
+	val taskForm = Form(
+		"label" -> nonEmptyText
+	)
+  
+	def index = Action {
+		Redirect(routes.Application.tasks)
+	}
+	
+	def tasks = Action {
+		Ok(views.html.index(Task.all(), taskForm))
+	}
+	
+	def newTask = TODO
+	
+	def deleteTask(id: Long) = TODO
+}
